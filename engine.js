e4e71d12df30819307583bc04619a7db8a4a4dc9
Lera <vbaptidanova@gmail.com>
2022-09-17 19:02:10 +0300

    ready

--- engine.js
+++ engine.js
@@ -5,18 +5,25 @@ window.addEventListener('load', function(){
     const panelDigitButtons = document.getElementById ('digit-panel');
 
     let number = 0;
+    let operation = '';
     let buffer = 0;
    
     panelDigitButtons.addEventListener('click', function(e){
         // console.log('Панель цифр');
         if (IsButton(e) === false) return;
-        if (e.target.dataset.button === 'clear'){
+        if (e.target.dataset.button === 'clear') {
             DisplayClear();
             ResetAll();
             return
         }
 
-        if (e.target.dataset.button === 'backspace');
+        if (e.target.dataset.button === 'backspace') {
+            number = Backspace(number);
+            DisplayShow(number);
+            return;
+        }
+           
+
         number = Concatenation(number, e.target.dataset.button);
 
         DisplayShow(number);
@@ -26,6 +33,24 @@ window.addEventListener('load', function(){
     operationButtons.addEventListener('click', function(e){
         // console.log('Панель операций');
         if (IsButton(e) === false) return;
+
+        if (e.target.dataset.button === '=') {
+           number = operation !== '' ? Calculate(buffer+operation+number) : number;
+          
+           DisplayShow(number);
+           ResetAll();
+           return;
+        }
+
+        if (operation !== '') {
+            number = Calculate(buffer+operation+number);
+            DisplayShow(number);
+          
+        }
+
+        operation = e.target.dataset.button;
+        buffer = number;
+        number = 0;
 
     });
 
@@ -49,9 +74,16 @@ window.addEventListener('load', function(){
 
     function ResetAll(){
         number = 0;
+        operation = '';
+        buffer = 0;
     }
 
     function Backspace(number){
+        return Number(number.toString().slice(0, -1));
 
+    }
+
+    function Calculate(executionStack){
+        return eval(executionStack)
     }
 })
